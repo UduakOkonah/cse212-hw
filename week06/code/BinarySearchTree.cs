1e440bd5e6ddcd1c2cbc544f2fb8b079dfde677c
UduakOkonah <uduakobongokonah@gmail.com>
2025-08-05 15:11:32 +0100

    Week06

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -81,6 +81,12 @@ public class BinarySearchTree : IEnumerable<int>
     private void TraverseBackward(Node? node, List<int> values)
     {
         // TODO Problem 3
+        if (node is not null)
+        {
+            TraverseBackward(node.Right, values);  // visit right first
+            values.Add(node.Data);                 // then current node
+            TraverseBackward(node.Left, values);   // then left
+        }
     }
 
     /// <summary>
